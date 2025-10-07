6edcef9df1fd152bcb3662930785f68e5719ab8b
tbarlz <tbarland77@hotmail.com>
2025-10-07 17:38:22 -0400

    initial commit from spring initialzer

---
+++ src/main/java/io/github/tbarland/obscura/ObscuraApplication.java
@@ -0,0 +1,13 @@
+package io.github.tbarland.obscura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObscuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObscuraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tbarland/obscura/ObscuraApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tbarland.obscura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObscuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
