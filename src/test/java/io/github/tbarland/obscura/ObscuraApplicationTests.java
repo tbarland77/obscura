49beff0d5511f3a45e930c5209d90fad116561eb
tbarlz <tbarland77@hotmail.com>
2025-10-07 22:22:25 -0400

    add spotless for easy formatting rules and fail the build when the formatting check fails

--- src/test/java/io/github/tbarland/obscura/ObscuraApplicationTests.java
+++ src/test/java/io/github/tbarland/obscura/ObscuraApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ObscuraApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
